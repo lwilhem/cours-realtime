430c4866d4a039b5afdb8bb68fc4caff5a37f065
lwilhem <wilhem.lecanu@edu.devinci.fr>
2024-05-13 10:30:48 +0200

    backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,14 @@
 import express from "express"
+import cors from "cors"
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.json('ip address: http://' + "127.0.0.1" +':'+ PORT);    
+})
+
+export default app
