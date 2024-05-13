592ea5680493786a95994130f3b8f35281e94395
lwilhem <wilhem.lecanu@edu.devinci.fr>
2024-05-13 11:12:32 +0200

    socket.io

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,20 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+
+
+socket.on("connect", () => {
+  console.log("Connected to the server");
+});
+
+function createRoom(_e: MouseEvent) {
+  socket.emit("create_room");
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <button @click="createRoom">Create room</button>
+  <h1>Hello World !</h1>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
